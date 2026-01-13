f6da8abca1a5bb86f91d5bd063c9a8de11801b5e
mohamedosama11893 <mohamedosama11893@gmail.com>
2026-01-13 22:54:07 +0200

    building the gui with tkinter and loading the images of symbols that will be used with glob and PIL libraries and setting the default values for the score

--- Tic_Tac_Toe_with_GUI_two_players.py
+++ Tic_Tac_Toe_with_GUI_two_players.py
@@ -16,3 +16,32 @@ from PIL import Image, ImageTk
 #      - switch player turn and update player label
 # 7) The GUI runs inside window.mainloop().
 # ================================================================
+
+
+# ----------- GUI -----------
+
+window = tk.Tk()
+window.title("X O Game by Mohamed Osama")
+# window.geometry("420x790")
+window.resizable(False, False)
+
+# load image assets
+assets = load_game_assets("characters")
+
+# resize and convert images using PIL then make PhotoImage for Tkinter
+x_img = Image.open(assets["X"])
+x_img = x_img.resize((100, 100))
+x_img = ImageTk.PhotoImage(x_img)
+
+o_img = Image.open(assets["O"])
+o_img = o_img.resize((100, 100))
+o_img = ImageTk.PhotoImage(o_img)
+
+# important state variables
+players = ["X", "O"]
+player = random.choice(players)
+
+user_score = tk.IntVar(value=0)
+computer_score = tk.IntVar(value=0)
+result_text = tk.StringVar(value="")
+score_text = tk.StringVar(value=f"You: {user_score.get()} Computer: {computer_score.get()}")
